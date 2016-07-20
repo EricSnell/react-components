f86b7bfe4a6d668aaaefe06ead637442bcccc8a7
Eric Snell <eric_snell21@yahoo.com>
2016-07-20 12:06:48 -0400

    modified jsx

--- index.jsx
+++ index.jsx
@@ -1,49 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-/*====Stateless Version=====*/
-// var Person = function(props) {
-//     console.log(props);
-//     return (
-//         <div className="person">
-//             <div className="person-name">{props.name}</div>
-//             <img className="person-img" src={props.imageUrl} />
-//             <div className="person-job">
-//                 {props.job}
-//             </div>
-//         </div>
-//     );
-// };
-
 /*====Stateful Version=====*/
-var Person = React.createClass({
-    getInitialState: function() {
-        return {
-            highlight: false
-        };
-    },
-    onClick: function() {
-        console.log(this.state.highlight);
-        this.setState({
-            highlight: !this.state.highlight
-        });
-    },
-    render: function() {
-        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
-        return (
-            <div className={classes}>
-                <div className="person-name">{this.props.name} onClick={this.onClick}</div>
-                <img className="person-img" src={this.props.imageUrl} />
-                <div className="person-job">
-                    {this.props.job}
-                </div>
+// var Person = React.createClass({
+//     getInitialState: function() {
+//         return {
+//             highlight: false
+//         };
+//     },
+//     onClick: function() {
+//         console.log(this.state.highlight);
+//         this.setState({
+//             highlight: !this.state.highlight
+//         });
+//     },
+//     render: function() {
+//         var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+//         return (
+//             <div className={classes}>
+//                 <div className="person-name" onClick={this.onClick}>{this.props.name}</div>
+//                 <img className="person-img" src={this.props.imageUrl} />
+//                 <div className="person-job">
+//                     {this.props.job}
+//                 </div>
+//             </div>
+//         );
+//     }
+// });
+
+/*====Stateless Version=====*/
+
+// We pull the 'name', 'imageUrl', and 'job' from the props argument that is
+// passed into the component
+var Person = function(props) {
+    console.log(props);
+    return (
+        <div className="person">
+            <div className="person-name">{props.name}</div>
+            <img className="person-img" src={props.imageUrl} />
+            <div className="person-job">
+                {props.job}
             </div>
-        );
-    }
-});
+        </div>
+    );
+};
 
-
-
+// Setting a default image for the prop if not supplied using the defaultProps attribute
 Person.defaultProps = {
     imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
 };
